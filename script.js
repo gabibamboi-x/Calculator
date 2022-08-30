c0ef2eebd74b3133890ef5a08073b86d163421d7
Gabriel Bamboi <gabi.bamboi_x@icloud.com>
2022-08-30 20:34:10 +0200

    Implement the functionality of most buttons

--- script.js
+++ script.js
@@ -6,20 +6,82 @@ const basicBtn = document.querySelector('.Buttons');
 
 let s = 0;
 for (let i = 1; i < 5; i++) {
+    // create the 4 divs for our columns
     let btnColumn = document.createElement('div');
     btnColumn.setAttribute('class', 'column' + i);
     basicBtn.appendChild(btnColumn);
 
+    // create the actual buttons of our calculator,
+    // also adding their symbol (as value and textContent)
     for (let j = 1; j < 6; j++) {
         let btn = document.createElement('button');
-        btn.setAttribute('id', 'btn' + symbols[s]);
         btn.setAttribute('class', 'btn');
+        btn.value = symbols[s];
         btn.textContent = symbols[s];
-        btn.style.backgroundColor = 'lightblue';
         btnColumn.appendChild(btn);
         s++;
     }
 };
+
+// get the display element
+const displayValue = document.getElementById('displayWindow');
+// select all buttons
+const button = document.querySelectorAll('.btn');
+// create a variable to keep track of the number that has to be shown to the user
+let currentNumber = '';
+// create a variable that's needed to store the whole operation
+let currentValue = '';
+
+// Add an Event Listener for each button that does different things based on their value.
+button.forEach(el => el.addEventListener('click', () => {
+    // if the value is not NaN it's a number which is displayed when clicked on it
+    if (!isNaN(el.value)) {
+        // add the number to the main string 
+        currentValue += el.value;
+        // add the number to the current number that is displayed 
+        currentNumber += el.value;
+        // print the number on screen
+        display();
+    } else if (el.value === 'AC') {
+        // reset everything when AC is pressed
+        currentValue = '';
+        currentNumber = '';
+        // show nothing / clear the calculator's display
+        display();
+    } else if (el.value === '±') {
+        // to get the negative number we simply subtract the double of it
+        // it also works from negative to positive ;)
+        currentValue -= currentValue * 2;
+        currentNumber -= currentNumber * 2;
+        display();
+    } else if (el.value === '%') {
+        // for the percentage we simply show that number divided by 100
+        currentNumber = currentNumber / 100;
+        display();
+    } else if (el.value === '=') {
+        // TODO operate();
+    } else if (el.value === '.') {
+        // in order to avoid having 2 dots in our number we check first if there's already one
+        // if not we go ahead and add one
+        if (!currentNumber.includes('.')) {
+            currentValue += el.value
+            currentNumber += el.value;
+            display()
+        }
+    } else if (el.value === 'DEL') {
+        // everytime the DEL button is pressed the last char in our number is removed
+        currentNumber = currentNumber.slice(0, -1);
+        currentValue  = currentValue.slice(0, -1);
+        display();
+    } else {
+        // add the operators to the operating string
+        currentValue += ' ' + el.value + ' ';
+        // reset the display number to start over when typing after an operator
+        currentNumber = '';
+        // replacing the symbols with operators understood by JS
+        currentValue = currentValue.replace('×', '*').replace('÷', '/');
+    }
+}));
 
 
 function add(a, b) {
@@ -48,4 +110,8 @@ function operate(operator, firstNumber, secondNumber) {
     }
     
     return divide(firstNumber, secondNumber);
+}
+
+function display() {
+    displayValue.textContent = currentNumber;
 }
